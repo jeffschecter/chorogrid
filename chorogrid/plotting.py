c06430abfb376e0328dcc89c5b84ab50bd5ad992
jeffschecter <jeff.schecter@stitchfix.com>
2018-09-13 15:53:09 -0700

    counties

--- chorogrid/plotting.py
+++ chorogrid/plotting.py
@@ -9,9 +9,13 @@ from chorogrid import Chorogrid
 DEFAULT_COLORS = [
     '#b35806', '#f1a340', '#fee0b6', '#d8daeb', '#998ec3', '#542788']
 DEFAULT_COMPLEMENTS = ['#e0e0e0', '#101010']
-SPEC_PATH = os.path.join(
+STATE_FILEPATH = os.path.join(
     os.path.dirname(os.path.realpath(__file__)),
     "databases", "usa_states.csv")
+COUNTY_FILEPATH = os.path.join(
+    os.path.dirname(os.path.realpath(__file__)),
+    "databases", "usa_counties.csv")
+COUNTY_IDS = set(pd.read_csv(COUNTY_FILEPATH).fips_integer.unique())
 
 
 STATE_ABBREVS = {
@@ -94,7 +98,7 @@ def plot(
     cbin.calc_complements(0.0, *complements)
 
     # Choropleth
-    cg = Chorogrid(SPEC_PATH, states, cbin.colors_out)
+    cg = Chorogrid(STATE_FILEPATH, states, cbin.colors_out)
     cg.set_title(title, font_dict=font)
     if quantile:
         labels = HEXILE_LABELS
@@ -111,4 +115,36 @@ def plot(
         "map": cg.draw_map
     }[shape]
     draw_fn(spacing_dict=spacing)
+    return cg.done(show=True)
+
+
+def plot_counties(
+        fips, values, title="", legend="",
+        colors=DEFAULT_COLORS, complements=DEFAULT_COMPLEMENTS,
+        font={}, spacing={}, decimals=1, quantile=False):
+    fips, values = zip(*[
+        (fip, value) for fip, value in zip(fips, values)
+        if fip in COUNTY_IDS])
+    if quantile:
+        values = pd.qcut(values, 6, [-20, -10, -1, 1, 10, 20])
+
+    # Colors
+    cbin = Colorbin(values, colors, proportional=True, decimals=None)
+    cbin.set_decimals(decimals)
+    cbin.recalc(fenceposts=True)
+    cbin.calc_complements(0.0, *complements)
+
+    # Choropleth
+    cg = Chorogrid(
+        COUNTY_FILEPATH, fips, cbin.colors_out,
+        id_column="fips_integer")
+    cg.set_title(title, font_dict=font)
+    if quantile:
+        labels = HEXILE_LABELS
+    else:
+        labels = cbin.labels
+    cg.set_legend(cbin.colors_in, labels, title=legend)
+
+    # Draw
+    cg.draw_map(spacing_dict=spacing)
     return cg.done(show=True)
